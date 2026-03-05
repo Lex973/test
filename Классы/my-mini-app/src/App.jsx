7c3bde26fe6277b72a369ac36f3ac9c60e12ac97
Lex973 <lenya.kurinin@mail.ru>
2026-03-05 22:46:58 +0300

    wow

--- Классы/my-mini-app/src/App.jsx
+++ Классы/my-mini-app/src/App.jsx
@@ -5,16 +5,17 @@ import WebApp from '@twa-dev/sdk'
 function App() {
     WebApp.ready()
     const user = WebApp.initDataUnsafe.user
-    console.log('user: ', user)
+
+    const [userName, setUserName] = useState('')
 
     function handleClick() {
-        const name = propmt('Введите имя: ')
-        alert(`Привет: ${name}`)
+        alert(`Привет: ${userName}`)
     }
 
   return (
     <>
         <h1>Привет!!</h1>
+        <input value={userName} onChange={(e) => setUserName(e.target.value)} type="text" placeholder='Введите имя'/>
         <button onClick={handleClick}>Click me!</button>
     </>
   )
