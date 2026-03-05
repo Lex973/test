05d55f05f263cb124d78d2359642b6544a4b6746
Lex973 <lenya.kurinin@mail.ru>
2026-03-05 22:39:59 +0300

    add h1

--- Классы/my-mini-app/src/App.jsx
+++ Классы/my-mini-app/src/App.jsx
@@ -5,10 +5,15 @@ import WebApp from '@twa-dev/sdk'
 function App() {
     WebApp.ready()
     const user = WebApp.initDataUnsafe.user
-    console.log(user)
+    console.log('user: ', user)
+
+    function handleClick() {
+        alert('clicked')
+    }
   return (
     <>
         <h1>Привет!!</h1>
+        <button onClick={handleClick}></button>
     </>
   )
 }
