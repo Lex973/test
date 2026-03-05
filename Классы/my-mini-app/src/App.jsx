a15ae50826997e6b51d3a5a672d494a702c66119
Lex973 <lenya.kurinin@mail.ru>
2026-03-05 22:23:14 +0300

    test

---
+++ Классы/my-mini-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import WebApp from '@twa-dev/sdk'
+
+function App() {
+    WebApp.ready()
+    const user = WebApp.initDataUnsafe.user
+    console.log(user)
+  return (
+    <>
+        <h1>Привет!</h1>
+    </>
+  )
+}
+
+export default App
